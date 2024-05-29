59d489aa929939be61723f0d7256d1ca7c932981
jelle De Beukeleer <jelle.debeukeleer@student.uantwerpen.be>
2024-05-29 15:18:47 +0200

    added image_masking

--- utils/image_tiling.py
+++ utils/image_tiling.py
@@ -16,3 +16,18 @@ def tile_images(images: List[torch.Tensor]):
             full_image = torch.cat((full_image, image), dim=2)
         first_loop = False
     return full_image
+
+
+def mask_tiled_images(images: torch.Tensor, concat_count: int, preserved_image: int):
+    assert preserved_image < concat_count, f"invalid image index {preserved_image} for concat of {concat_count} images"
+    assert images.shape[2] % concat_count == 0, f"image of shape {images.shape} should have dimension divisible by {concat_count}"
+
+    mask = torch.zeros_like(images)
+    separate_image_height = images.shape[1] / concat_count
+
+    starting_x, ending_x = [0, images.shape[2]]
+    starting_y, ending_y = [(preserved_image + i) * separate_image_height for i in range(2)]
+
+    mask[:, int(starting_y):int(ending_y), int(starting_x):int(ending_x)] = 1
+
+    return images * mask
