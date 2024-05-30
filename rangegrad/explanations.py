reverted  incorrect gradient backprop

rangegrad/explanations.py
@@ -51,7 +51,7 @@ def rangegrad_explanation(
 
     f = (bounds[2] - bounds[0])
 
-    f.backward(torch.ones_like(f))
+    f.backward(OH)
     grad_diff = diff_matrix.grad.data.squeeze().detach()
 
     return grad_diff